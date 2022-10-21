5f76dacdb134d3e927fe35bc78e6815a8b11fd76
Mous <mousahmed00@gmail.com>
2022-10-21 15:42:13 +0300

    First phase creating the fragment before & commented nav bar (not completed)

---
+++ app/src/main/java/com/example/foodmedia/FriendsPage.java
@@ -0,0 +1,14 @@
+package com.example.foodmedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FriendsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_page);
+    }
+}
